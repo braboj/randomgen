03988c517a4964ddd4b248bc17228b2af946345c
Branimir Georgiev <braboj@gmail.com>
2024-04-07 12:16:42 +0300

    (#13) - Update the docstrings of the histogram class and some minor changes

--- randomgen/histogram.py
+++ randomgen/histogram.py
@@ -8,7 +8,15 @@ from randomgen.errors import (
 
 
 class Histogram(dict):
-    """ Helper class to build a histogram from a list of numbers """
+    """ Helper class to build a histogram from a list of numbers
+
+    Attributes:
+        _numbers: A list of numbers.
+        _counter: A Counter object to count the occurrences of each number.
+        _total: The total number of elements in the list.
+        _probabilities: A list of probabilities for each number.
+
+    """
 
     def __init__(self):
         super().__init__()
@@ -18,14 +26,40 @@ class Histogram(dict):
         self._probabilities = ()
 
     def from_dict(self, histogram):
+        """ Set the histogram from a dictionary.
+
+        Args:
+            histogram: A dictionary of numbers and probabilities.
+
+        Returns:
+            self: The instance of the class.
+
+        """
+
         self.update(histogram)
         return self
 
     def set_numbers(self, numbers):
+        """ Set the numbers to build the histogram.
+
+        Args:
+            numbers: A list of numbers.
+
+        Returns:
+            self: The instance of the class.
+
+        """
+
         self._numbers = numbers
         return self
 
     def validate_numbers(self):
+        """ Validate the numbers.
+
+        Returns:
+            self: The instance of the class.
+
+        """
 
         # Check if the numbers is None
         if self._numbers is None:
@@ -50,12 +84,31 @@ class Histogram(dict):
         return self
 
     def validate(self):
+        """ Validate the numbers and probabilities.
+
+        Returns:
+            self: The instance of the class.
+
+        """
+
         self.validate_numbers()
         return self
 
     def calc(self):
+        """ Calculate the histogram.
+
+        Returns:
+            self: The instance of the class.
+
+        """
+
+        # Count the occurrences of each number
         self._counter = Counter(self._numbers)
+
+        # Calculate the total number of elements
         self._total = sum(self._counter.values())
+
+        # Update self with the histogram parameters
         self.update(
             {
                 num: count / self._total
@@ -64,12 +117,15 @@ class Histogram(dict):
             }
         )
 
-        hist = dict(sorted(self.items()))
-        self.update(hist)
         return self
 
 
+###############################################################################
+# Examples
+###############################################################################
+
 if __name__ == "__main__":
+
     import random
 
     # Generate 1000 random numbers
