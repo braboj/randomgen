c58b47cc0c90dab4c970cb14a08a58e683c67af5
Branimir Georgiev <braboj@gmail.com>
2024-04-07 15:59:26 +0300

    (#13) - Add a setup file in order to increment the versions of the product.

---
+++ setup.py
@@ -0,0 +1,14 @@
+LONG_DESCRIPTION = """
+RandomGen is a project that provides an easy-to-use and easy-to-extend
+framework for generating random data. 
+
+The web application runs as a web server and provides a RESTful API for
+generating random data.
+
+""".strip()
+
+SHORT_DESCRIPTION = """
+An application to generate random numbers using REST API.""".strip()
+
+VERSION = '0.0.0.0'
+URL = 'https://github.com/braboj/randomgen'
